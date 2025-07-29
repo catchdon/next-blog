a45732ebe6ac49d1e5095098aa6b74773f5b3bee
박종현 <bagjonghyeon@bagjonghyeon-ui-MacBookAir.local>
2025-07-30 04:16:15 +0900

    가허3411461

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,29 @@ export default function Document() {
   return (
     <Html lang="ko">
       <Head>
-        {/* 필요한 경우 폰트, favicon, 기타 전역 meta를 여기 추가 가능 */}
+        {/* 기본 파비콘 */}
+        <link rel="icon" href="/favicon.ico" />
+
+        {/* SVG 파비콘(있으면) */}
+        <link rel="icon" type="image/svg+xml" href="/icon0.svg" />
+
+        {/* PNG 파비콘(있는 경우) */}
+        <link rel="icon" type="image/png" href="/icon1.png" />
+
+        {/* 애플 터치 아이콘 */}
+        <link rel="apple-touch-icon" href="/apple-icon.png" />
+
+        {/* 웹 앱(안드로이드/크롬/홈화면) 아이콘 */}
+        <link rel="icon" type="image/png" sizes="192x192" href="/web-app-manifest-192x192.png" />
+        <link rel="icon" type="image/png" sizes="512x512" href="/web-app-manifest-512x512.png" />
+
+        {/* Manifest */}
+        <link rel="manifest" href="/manifest.json" />
+
+        {/* 브라우저/OS용 색상 등 */}
+        <meta name="theme-color" content="#ffffff" />
+        <meta name="application-name" content="게임교수" />
+        <meta name="apple-mobile-web-app-title" content="게임교수" />
       </Head>
       <body>
         <Main />
